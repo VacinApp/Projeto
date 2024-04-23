230ff8cf15e7d70e5866a6ee528e1bb7bd9113fb
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-04-23 19:55:03 -0300

    feat: nova funcionalidade de rota dinâmica

--- src/app/[detalhe]/page.js
+++ src/app/[detalhe]/page.js
@@ -1,12 +1,13 @@
 import Detalhe from "../components/Detalhe/Detalhe";
 import FooterNav from "../components/FooterNav/FooterNav";
 import Navbar from "../components/Navbar/Navbar";
+import vacs from "../json/vacs.json";
 
-export default function PagDetalhe() {
+export default function PagDetalhe({searchParams}) {
 	return (
 		<div>
 			<Navbar />
-			<Detalhe />
+      <Detalhe id={searchParams.id} />
 			<FooterNav />
 		</div >
 	);
--- src/app/components/Detalhe/Detalhe.jsx
+++ src/app/components/Detalhe/Detalhe.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 
 import styles from'./detalhe.module.css';
+import vacs from 'O:/repositorio/Projeto/src/app/json/vacs.json'
 
-const vacina = {
-  nome: 'BCG',
-  descricao: 'A vacina contra a tuberculose é o BCG (bacilo de Calmette & Guérin) liofilizado, obtido por atenuação do Mycobacterium bovis, apresentada em ampolas com múltiplas doses.',
-  imagem: '/assets/img/vacinas/bcg.png',
-}
 
-const Detalhe = () => {
+const Detalhe = ({id}) => {
+  const vacina = {
+    nome: vacs[id].nome,
+    descricao: vacs[id].descricao,
+    imagem: vacs[id].imagem,
+  }
   return (
     <section className={styles.detalheContainer}>
       <img className={styles.detalheImg} src={vacina.imagem} alt={vacina.nome} />
       <div className={styles.detalheConteudo}>
         <h1>{vacina.nome}</h1>
         <p>{vacina.descricao}</p>
-
         <div className={styles.botao}>
           <Link
             href={'/localizacao'}
